8c264ba0f826b15afce3b09e5cb428a82d4f508f
Philipp S <spce.marine.games@googlemail.com>
2019-10-10 21:17:02 +0200

    save some changes

---
+++ build/precache-manifest.e441e2d4ece49e614c275b396c2bf9ee.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3867b41c5abb1684b30ecb8fceabd9ef",
+    "url": "/toornament-smart-schedule/index.html"
+  },
+  {
+    "revision": "6230dc83e37b8cbdb60b",
+    "url": "/toornament-smart-schedule/static/js/2.a04c12aa.chunk.js"
+  },
+  {
+    "revision": "9fc268868a02db378f98",
+    "url": "/toornament-smart-schedule/static/js/main.4d4be1d8.chunk.js"
+  },
+  {
+    "revision": "0ffde8757aeab7752888",
+    "url": "/toornament-smart-schedule/static/js/runtime-main.12d142a4.js"
+  }
+]);
